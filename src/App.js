35d4c873b1d67d5f6c548a0eb2bb2f32c969858a
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2019-08-01 21:54:24 -0400

    Add Palette

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import styled from 'styled-components'
+import { color, Box, Serif } from '@artsy/palette'
 
 export default function App() {
-  return <Main>Hellooo</Main>
+  return (
+    <Main>
+      <Box m={1}>
+        <Serif size="6">Hellooo</Serif>
+      </Box>
+    </Main>
+  )
 }
 
 const Main = styled.main`
   padding: 1em;
+  background: ${color('black10')};
 `
